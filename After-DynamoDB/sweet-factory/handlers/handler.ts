1e463d885822807e309d2554a033898d3ca86a63
Daniel Aniszkiewicz <daniel.aniszkiewicz@gmail.com>
2023-05-06 20:01:07 +0200

    Add After-DynamoDB

---
+++ After-DynamoDB/sweet-factory/handlers/handler.ts
@@ -0,0 +1,13 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const hello = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Welcome to the Sweet Factory!",
+      input: event,
+    }),
+  };
+};
