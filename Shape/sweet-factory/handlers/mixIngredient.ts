b6264370a53e500aad1ce63a71252068caddf917
Daniel Aniszkiewicz <daniel.aniszkiewicz@gmail.com>
2023-05-20 15:47:22 +0200

    Shape

---
+++ Shape/sweet-factory/handlers/mixIngredient.ts
@@ -0,0 +1,21 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+const getRandomMixingTime = () => {
+  const minTime = 10; // 10 seconds
+  const maxTime = 360; // 6 minutes in seconds
+
+  return Math.floor(Math.random() * (maxTime - minTime + 1) + minTime);
+};
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const mixingTime = getRandomMixingTime();
+
+  return {
+    statusCode: 200,
+    body: {
+      waitTime: mixingTime,
+    },
+  };
+};
