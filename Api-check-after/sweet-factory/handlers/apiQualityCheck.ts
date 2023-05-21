ca80de7f1832e0dfe346043653dd2ee9f22e1b56
Daniel Aniszkiewicz <daniel.aniszkiewicz@gmail.com>
2023-05-21 15:06:37 +0200

    fix

--- Api-check-after/sweet-factory/handlers/apiQualityCheck.ts
+++ Api-check-after/sweet-factory/handlers/apiQualityCheck.ts
@@ -8,7 +8,7 @@ import { validate as uuidValidate } from "uuid";
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const batchId = JSON.parse(event.body)?.batchId;
+  const batchId = event.body?.batchId;
 
   if (batchId && uuidValidate(batchId)) {
     return { validApiQualityCheck: true };
